43c00c72418f83192e880480ee9994166c39ece5
Zain Ul Abideen <66625704+Zainapplicon152@users.noreply.github.com>
2020-08-31 12:21:19 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
+  name = 'zain';
+  currentVal = '';
+  disableButton = true;
+
+  obj = {name: 'zain', age: 22};
+  arr = ['zain', '22', 'love'];
+
+  // tslint:disable-next-line:typedef
+  getName() {
+    return this.name;
+  }
+
+  // tslint:disable-next-line:typedef
+  getMyName() {
+    alert('zain');
+  }
+
+  // tslint:disable-next-line:typedef
+  myEvent(evt) {
+    console.warn(evt);
+  }
+
+  getVal(val) {
+    console.warn(val);
+    this.currentVal = val;
+  }
+
+  enablebutton() {
+    this.disableButton = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+import {LoginComponent} from './users/login/login.component';
+import {SignupComponent} from './users/signup/signup.component';
+import {UsersModule} from './users/users.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
