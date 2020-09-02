9489daadd266ce3066e33bd15092b5823f95e899
Zain Ul Abideen <66625704+Zainapplicon152@users.noreply.github.com>
2020-09-01 22:16:01 -0800

    if, for, switch in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,37 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular';
   name = 'zain';
   currentVal = '';
   disableButton = true;
+  show = 'yes';
+  color = 'green';
+  users = [
+    {
+      name: 'zain',
+      age: 22,
+      email: 'zain@gmail.com',
+    },
+    {
+      name: 'zain2',
+      age: 22,
+      email: 'zain@gmail.com',
+    },
+    {
+      name: 'zain3',
+      age: 22,
+      email: 'zain@gmail.com',
+    },
+    {
+      name: 'zain4',
+      age: 22,
+      email: 'zain@gmail.com',
+    }
+  ];
 
   obj = {name: 'zain', age: 22};
   arr = ['zain', '22', 'love'];
